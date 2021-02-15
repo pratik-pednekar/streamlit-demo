1b1c1139babfa1393e9846ae035a9e62203bfc9f
pratik-pednekar <62905876+pratik-pednekar@users.noreply.github.com>
2021-02-15 17:11:22 -0500

    Update app.py
    added !

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 def app():
-	st.title("Minimal Streamlit Template with changes")
+	st.title("Minimal Streamlit Template with changes!")
 	# And write the rest of the app inside this function!
 
 if __name__ == '__main__':
